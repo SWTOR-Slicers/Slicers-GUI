5035808d8b625d1fbd7a136bf1dbb2544097be49
Tormak9970 <63308171+Tormak9970@users.noreply.github.com>
2021-06-27 12:55:28 -0400

    file changer UI complete

--- app/src/js/utilities/fileChanger/FileChanger.js
+++ app/src/js/utilities/fileChanger/FileChanger.js
@@ -4,8 +4,33 @@
 const live = document.getElementById('live');
 const pts = document.getElementById('pts');
 
+//assets folder variables
+const assetsFolderInput = document.getElementById('assetsFolderInput');
+const assetsFolderBrowseBtn = document.getElementById('assetsFolderBrowseBtn');
+
+//backup checkbox
+const createBackup = document.getElementById('createBackup');
+
+//preset variables
+const loadPre = document.getElementById('loadPre');
+const writPre = document.getElementById('writPre');
+
+const loadedPresetPath = document.getElementById('loadedPresetPath');
+
+const convPre = document.getElementById('convPre');
+
+//action variables
+const restoreBackup = document.getElementById('restoreBackup');
+const extrFile = document.getElementById('extrFile');
+const extrNode = document.getElementById('extrNode');
+
+const chngFiles = document.getElementById('chngFiles');
+
 //progress bar
 const progBar = document.getElementById('progBar');
+
+//change variables
+const addChange = document.getElementById('addChange');
 
 function verifyEntry(ent) {
     
---
+++ app/src/js/utilities/filechanger/FileEntry.js
@@ -0,0 +1,11 @@
+class FileEntry {
+    constructor(type, target, modded) {
+        this.type = type;
+        this.target = target;
+        this.modded = modded;
+    }
+
+    render() {
+
+    }
+}

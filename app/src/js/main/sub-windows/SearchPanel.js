68840654402d4b30b0fc2f61f01cb6a26bda4c4d
Tormak <63308171+Tormak9970@users.noreply.github.com>
2021-08-11 19:50:57 -0500

    more search panel work

--- app/src/js/main/sub-windows/SearchPanel.js
+++ app/src/js/main/sub-windows/SearchPanel.js
@@ -1,3 +1,5 @@
+const {EditorView} = require('@codemirror/view');
+
 export class SearchPanel {
     /**
      * A more stylish version of the codeMirror search panel.
@@ -16,26 +18,26 @@ export class SearchPanel {
         this.panel.id = "searchPanel";
         this.panel.innerHTML = `
             <div class="seg-cont" style="margin-bottom: 2px; margin-top: 4px;">
-                <input placeholder="Find" type="text" class="paths-field-input-alt">
-                <div class="num-res">No results</div>
-                <button id="moveUp" class="browse-paths__button-alt">
+                <input id="findInput" placeholder="Find" type="text" class="paths-field-input-alt">
+                <div id="matchesInfo" class="num-res" style="width: 61.84px;">No results</div>
+                <button id="moveUp" class="browse-paths__button-alt disabled">
                     <i class="fas fa-arrow-up"></i>
                 </button>
                 <input id="matchCase" class="over-btn" is="check-box"/>
                 <div class="num-res" style="margin-left: 5px;">match case</div>
-                <button id="closeSearc" class="browse-paths__button-alt" style="margin-left: auto;">
+                <button id="closeSearch" class="browse-paths__button-alt" style="margin-left: auto;">
                     <i class="fas fa-times"></i>
                 </button>
             </div>
             <div class="seg-cont" style="margin-top: 2px; margin-bottom: 4px;">
-                <input placeholder="Replace" type="text" class="paths-field-input-alt" style="margin-right: 34px;">
-                <button id="replaceOne" class="browse-paths__button-alt">
+                <input id="replaceInput" placeholder="Replace" type="text" class="paths-field-input-alt" style="margin-right: 34px;">
+                <button id="replaceOne" class="browse-paths__button-alt disabled">
                     <i class="fas fa-square"></i>
                 </button>
-                <button id="replaceAll" class="browse-paths__button-alt">
+                <button id="replaceAll" class="browse-paths__button-alt disabled">
                     <i class="fas fa-clone"></i>
                 </button>
-                <button id="moveDown" class="browse-paths__button-alt">
+                <button id="moveDown" class="browse-paths__button-alt disabled">
                     <i class="fas fa-arrow-down"></i>
                 </button>
                 <input id="regEx" class="over-btn" is="check-box"/>
@@ -47,15 +49,107 @@ export class SearchPanel {
 
 
         // add event listeners
+
+        //DOM variables
+        const findInput = document.getElementById('findInput');
+        this.findInput = findInput;
+        const replaceInput = document.getElementById('replaceInput');
+        const matchesInfo = document.getElementById('matchesInfo');
+
+        const replaceOne = document.getElementById('replaceOne');
+        const replaceAll = document.getElementById('replaceAll');
+
+        const moveUp = document.getElementById('moveUp');
+        const moveDown = document.getElementById('moveDown');
+
+        const matchCase = document.getElementById('matchCase');
+        const regEx = document.getElementById('regEx');
+
+        const closeSearch = document.getElementById('closeSearch');
+
+        this.matches = [];
+
+        // on keydown check for matches
+        findInput.addEventListener('input', (e) => {
+            this.matches = [];
+            this.matchIdx = 0;
+            let querey = e.currentTarget.value;
+            if (querey !== "") {
+                let docContents = this.view.state.doc.sliceString(0, this.view.state.doc.length);
+                if (regEx.checked) {
+                    // if regex apply find as regex
+                    let match;
+                    while ((match = querey.exec(docContents)) !== null) {
+                        this.matches.push({
+                            startIdx: match.index,
+                            endIdx: querey.lastIndex
+                        })
+                        console.log(`Found ${match[0]} start=${match.index} end=${querey.lastIndex}.`);
+                    }
+                } else {
+                    // if match case enfroce case match
+                    if (!matchCase.checked) {
+                        docContents = docContents.toLowerCase();
+                        querey = querey.toLowerCase();
+                    }
+                    // else just perform a regular search
+                    let index, startIndex;
+                    while ((index = docContents.indexOf(querey, startIndex)) > -1) {
+                        startIndex = index + querey.length;
+                        this.matches.push({
+                            startIdx: index,
+                            endIdx: startIndex
+                        });
+                    }
+                    
+                }
+            }
+            
+            if (this.matches.length > 0) {
+                matchesInfo.innerHTML = `1 of ${this.matches.length}`;
+                moveUp.classList.remove('disabled');
+                moveDown.classList.remove('disabled');
+                replaceOne.classList.remove('disabled');
+                replaceAll.classList.remove('disabled');
+
+                this.setSelection(this.matches[0]);
+            } else {
+                matchesInfo.innerHTML = "No results";
+                moveUp.classList.add('disabled');
+                moveDown.classList.add('disabled');
+                replaceOne.classList.add('disabled');
+                replaceAll.classList.add('disabled');
+            }
+        });
+
+        // on move down click move to next selection in matches list
+
+        // on move up click move to previous selection in matches list
+
+        // on close call this.hide()
+        closeSearch.addEventListener('click', (e) => { this.hide(); });
     }
 
     show() {
-        this.showing = true;
+        this.showing = true; 
         this.panel.style.top = '0px';
+        this.findInput.focus();
+        if (this.matches.length > 0) {
+            this.setSelection(this.matches[0]);
+        }
     }
-
     hide() {
         this.showing = false;
         this.panel.style.top = '-60px';
+        this.parent.focus();
+    }
+
+    setSelection(match) {
+        this.view.dispatch({
+            selection: {
+                anchor: match.startIdx,
+                head: match.endIdx
+            }
+        });
     }
 }
